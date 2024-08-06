1a72e7ed0d9cadaaf8027ea4db0f1bb98e1eec5a
Pratham  Hebbar <prathamhebbar2021@gmail.com>
2024-08-06 13:09:43 -0700

    starting again

--- app/page.js
+++ app/page.js
@@ -1,17 +1,9 @@
-import styled from "styled-components";
-import Background from "./components/Background";
-import TextSection from "./components/TextSection";
+
 
 export default function Home() {
   return (
-    <Wrapper className="Home">
-      <Background/>
-      <TextSection/>
-    </Wrapper>
+    <div>
+      
+    </div>
   );
 }
-
-const Wrapper = styled.div`
-  position: relative;
-  background: #1f1144;
-`;
