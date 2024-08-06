34d9efaf1ee34b8518ff3925593b10311758c3e5
Pratham  Hebbar <prathamhebbar2021@gmail.com>
2024-08-06 13:01:13 -0700

    landing page UI

--- app/page.js
+++ app/page.js
@@ -1,8 +1,17 @@
+import styled from "styled-components";
+import Background from "./components/Background";
+import TextSection from "./components/TextSection";
 
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <Wrapper className="Home">
+      <Background/>
+      <TextSection/>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  position: relative;
+  background: #1f1144;
+`;
