d1906a8338c0b9b950a83dbd84f95939522d5fdb
Hemanka <hkanoujia02@gmail.com>
2024-08-07 22:17:51 -0400

    added the chat ui and moved the components folders outside the app folder

--- app/page.js
+++ app/page.js
@@ -1,12 +1,16 @@
-'use client'
-import Animation from './components/Animation';
-import Wormhole from './components/Wormhole';
+// 'use client'
+// import Animation from '@/components/Animation';
+// import Wormhole from '@/components/Wormhole';
+
+import ChatBox from "@/components/ChatBox";
 
 export default function Home() {
   return (
-    <div style={{ position: 'relative', width: '100%', height: '100vh'}}>
-        <Animation/>
-        <Wormhole width='800px' height='750px'/>
-    </div>
+    // <div style={{ position: 'relative', width: '100%', height: '100vh'}}>
+    //      <Animation/>
+    //      <Wormhole width='800px' height='750px'/>
+    // </div>
+    // <div>Hello!!</div>
+    <ChatBox />
   );
 }
---
+++ components/ChatBox.js
@@ -0,0 +1,76 @@
+'use client'
+import { Box, Stack, Button, TextField, TextareaAutosize} from '@mui/material'
+import { useState } from 'react'
+import React from 'react'
+import DisplayText from './DisplayText'
+import SendText from './SendText'
+
+function ChatBox() {
+  const [messages, setMessages] = useState([
+    {
+      role: 'assistant',
+      content: "Hi! I'm the Headstarter support assistant. How can I help you today?",
+    },
+    {
+      role: 'user',
+      content: "Hi!",
+    },
+    {
+      role: 'assistant',
+      content: "Hello there",
+    },
+    {
+      role: 'assistant',
+      content: "Hello there",
+    },
+    {
+      role: 'assistant',
+      content: "Hello here",
+    },
+
+    {
+      role: 'user',
+      content: "Hi there!",
+    },
+    {
+      role: 'user',
+      content: "Hi here!",
+    },
+    {
+      role: 'user',
+      content: "Hi there!",
+    },
+    {
+      role: 'user',
+      content: "fuiewbifenfheifnien ienifneifn wenfiwe nifneifnienfi ewfne nwfniwe ife  fiweqnfi wnf nifnwein wieunf wqnfei weifniwef nen fwenqif newiufnw nfwifniewfi we",
+    },
+  ])
+
+  return (
+    <Box
+      width="90vw"
+      height="90vh"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      marginTop={2}
+      marginX={'auto'}
+    >
+      <Stack
+        direction={'column'}
+        marginTop={4}
+        width="100%"
+        height="100%"
+        border="1px solid black"
+        padding={1}
+        spacing={1.5}
+      >
+        <DisplayText messages={messages} />
+        <SendText messages={messages} setMessages={setMessages}/>
+      </Stack>
+    </Box>
+  )
+}
+
+export default ChatBox
