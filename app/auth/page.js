99181a2e2ed325ab45fd8b9a80f9875314a96a80
Hemanka <hkanoujia02@gmail.com>
2024-08-10 20:49:32 -0400

    created the sign up ui

--- app/auth/page.js
+++ app/auth/page.js
@@ -1,8 +1,12 @@
 import Login from "@/components/Login";
+import SignUp from "@/components/SignUp";
 import React from "react";
 
 function page() {
-  return <Login />;
+  return (
+    // <Login />
+    <SignUp />
+  );
 }
 
 export default page;
---
+++ components/SignUp.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+
+function SignUp() {
+  return (
+    <Box
+      width="100vw"
+      height="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      margin={"auto"}
+    >
+      <Box
+        width="50vw"
+        height="60vh"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        border={1}
+        borderRadius={2}
+      >
+        <Stack
+          spacing={3}
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Typography variant="h4">Sign Up</Typography>
+          <Stack
+            spacing={2}
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <TextField label="Username" variant="outlined" />
+            <TextField label="Password" variant="outlined" />
+          </Stack>
+          <Button variant="contained">Sign Up</Button>
+        </Stack>
+      </Box>
+    </Box>
+  );
+}
+
+export default SignUp;
